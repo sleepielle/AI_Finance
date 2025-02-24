32df3b68b59b43ef6c9ae12b16b57e582189fdfc
sleepielle <pggisselle@gmail.com>
2025-02-24 15:22:05 -0600

    feat: adding project setup, app layout, user auth, header component

---
+++ components/header.jsx
@@ -0,0 +1,55 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import { LayoutDashboard, PenBox } from "lucide-react";
+
+export const Header = () => {
+  return (
+    <header className="fixed top-0 w-full bg-white/90 backdrop-blur-md z-50 border-b">
+      <nav className="container mx-auto px-4 py-4 flex items-center justify-between ">
+        <Link href="/">
+          <Image
+            src={"/logo.png"}
+            alt="logo"
+            width={200}
+            height={60}
+            className="h-12 w-auto object-contain"
+          />
+        </Link>
+
+        <div className="flex items-center space-x-4">
+          <SignedIn>
+            <Link
+              href="/dashboard"
+              className="text-gray-600 hover:text-blue-600 flex items-center gap-2"
+            >
+              <Button variant="outline">
+                <LayoutDashboard size={1} />
+                <span className="hidden md:flex">Dashboard</span>
+              </Button>
+            </Link>
+
+            <Link href={"/transaction/create"}>
+              <Button className="flex items-center gap-2">
+                <PenBox size={18} />
+                <span className="hidden md:inline">Add Transaction</span>
+              </Button>
+            </Link>
+          </SignedIn>
+
+          <SignedOut>
+            <SignInButton>
+              <Button variant="outline">Log In</Button>
+            </SignInButton>
+          </SignedOut>
+
+          <SignedIn>
+            <UserButton appearance={{ elements: { avatarBox: "w-10 h-10" } }} />
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
+  );
+};
