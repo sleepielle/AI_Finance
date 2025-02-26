77705e8d12dd6ef7e28245268bb3a6baac239e1b
sleepielle <pggisselle@gmail.com>
2025-02-25 19:21:18 -0600

    feat. prisma integration

--- components/header.jsx
+++ components/header.jsx
@@ -4,8 +4,10 @@ import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
 import { LayoutDashboard, PenBox } from "lucide-react";
+import { checkUser } from "@/lib/checkUser";
 
-export const Header = () => {
+export const Header = async () => {
+  await checkUser();
   return (
     <header className="fixed top-0 w-full bg-white/90 backdrop-blur-md z-50 border-b">
       <nav className="container mx-auto px-4 py-4 flex items-center justify-between ">
